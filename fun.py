4c499e5530be20acda3227a2bc623f2a13a68ea9
Mundurujoana <mundujoana@gmail.com>
2022-05-31 15:54:56 +0300

    first commit

--- fun.py
+++
@@ -1,6 +0,0 @@
-front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-back_end = ['Node','Express', 'MongoDB']
-# After joining the lists in question 26.
-front_end + back_end
-full_Stack = front_end + back_end
-print(full_Stack)
